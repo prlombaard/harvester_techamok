9de4c0ece682f970b7ce5721ab234349a97253dd
Rudolph Lombaard <prlombaard@gmail.com>
2017-12-08 08:46:45 -0800

    Break out of loop when file being served by server is not images anymore

--- src/harvest_techamok.py
+++ src/harvest_techamok.py
@@ -2,22 +2,33 @@
 # Date create: 08 December 2017
 # Author: Rudolph Lombaard (prlombaard@gmail.com)
 
+
+
+# DONE: stop iteration if "end of range" of images reached.
+
+# TODO: make multithreaded / multiprocess to download more images at once
+# TODO: Automatically determine range of images to harvest
+# TODO: use logging module, NOT prints!!!
+
+
 import requests
 
 
 def main():
     # Main function
     print('Techamok Harvester')
-    # TODO: make multithreaded / multiprocess to download more images at once
-    # TODO: stop iteration if "end of range" of images reached.
-    # TODO: Automatically determine range of images to harvest
-    # TODO: use logging module, NOT prints!!!
-    base_url = f'http://www.techamok.com/pics//17/dec/best'
-    for i in range(269, 277):
-        url = f'{base_url}/best{i:03}.jpg'
+    year = 17
+    base_url = f'http://www.techamok.com/pics'
+    # base_url = f'http://www.techamok.com/pics//17/dec/best'
+    months = ['jan', 'feb', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov','dec']
+    month_index = -1
+    start_index = 1
+    stop_index = 275
+    for i in range(start_index, stop_index):
+        url = f'{base_url}//{year}/{months[month_index]}/best/best{i:03}.jpg'
         filename = url.rsplit('/', 1)[1]
         # TODO: Check if output folder exists before writing there, if it does not exist create one
-        filename = f'../images/{filename}'
+        filename = f'../images/{year}/{months[month_index]}/{filename}'
         print(f'Trying to download content at URL {url}')
         r = requests.get(url, allow_redirects=True)
         # print(r.content[0:10])
@@ -29,6 +40,7 @@ def main():
             print(f'Written {url} to disk at {filename}')
         else:
             print(f'Skipping {url}, REASON = content from server not an image')
+            break   # Break out of loop, assuming that end of range have been reached
     
 
 if __name__ == '__main__':
